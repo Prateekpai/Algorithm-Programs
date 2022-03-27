c98e3b42f4d5fea37d38968871e6ee289bf7754b
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-27 10:48:45 +0530

    Welcome to Algorithm Programs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AlgorithmProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcometo to Algorithm Programs");
+        }
+    }
+}
