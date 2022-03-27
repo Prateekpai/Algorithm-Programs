e0be150d574a72a1a87d679a56445262085885ae
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-27 10:53:08 +0530

    BubbleSort Problem

---
+++ BubbleSort.cs
@@ -0,0 +1,44 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSortProblem
+{
+    public class BubbleSort
+    {
+        public Node Root { get; set; }
+
+        public bool Add(int value)
+        {
+            Node before = null, after = Root;
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                    after = after.LeftNode;
+                else if (value > after.Data)
+                    after = after.RightNode;
+                else
+                {
+                    return false;
+                }
+            }
+
+            Node newNode = new Node();
+            newNode.Data = value;
+
+            if (this.Root == null)
+            {
+                this.Root = newNode;
+            }
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+            return true;
+        }
+    }
+}
